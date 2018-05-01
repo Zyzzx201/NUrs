614d03c885f7d4d94d4e057fb92eae485e761782
Zyzzx201 <36744908+Zyzzx201@users.noreply.github.com>
2018-05-01 21:05:12 +0200

    Add files via upload
    added the rest of the CRUD for BILL table and added the rest of the crud of table week

--- userside mostafa/BillClass.php
+++ userside mostafa/BillClass.php
@@ -3,12 +3,15 @@ require_once("db.php");
 class Bill{
     public $id;
     public $parent_id;
-    public $payment_o_id;
-    public $value;
+    public $payment_id;
+    public $price;
+    public $discount;
+    public $time;
 
 	public function insert(){
         $DBObject = new DB();
-        $sql = "INSERT INTO pm_o_v(parent_id, payment_o_id, value) VALUES ('".$this->parent_id."','".$this->payment_o_id."','".$this->value."')";
+        $sql = "INSERT INTO bill (parent_id, payment_id, price, discount) VALUES ('".$this->parent_id."','".$this->payment_id."','".$this->price."',
+        '".$this->discount."')";
         $DBObject->connect();
         $DBObject->execute($sql);
         $DBObject->disconnect();
@@ -16,21 +19,23 @@ class Bill{
 
     public function select(){
         $DBObject = new DB();
-        $sql = "SELECT * FROM pm_o_v where id = '".$this->id."' ";
+        $sql = "SELECT * FROM bill where id = '".$this->id."' OR parent_id = '".$this->parent_id."' OR '".$this->payment_id."'";
         $DBObject->connect();
         $result =$DBObject->execute($sql);
         while ($row = mysqli_fetch_array($result)){
             echo $row['id'];
             echo $row['parent_id'];
-            echo $row['payment_o_id'];
-            echo $row['value'];
+            echo $row['payment_id'];
+            echo $row['price'];
+            echo $row['discount'];
+            echo $row['time'];
            }
         $DBObject->disconnect();
         }
 
     public function update(){
         $DBObject = new DB();
-        $sql = "UPDATE pm_o_v SET parent_id = '".$this->parent_id."', payment_o_id ='".$this->payment_o_id."',value = '".$this->value."'  WHERE id = '".$this->id."'";
+        $sql = "UPDATE bill SET parent_id = '".$this->parent_id."', payment_id ='".$this->payment_id."',price = '".$this->price."' , discount = '".$this->discount."' WHERE id = '".$this->id."'";
         $DBObject->connect();
         $DBObject->execute($sql);
         $DBObject->disconnect();
@@ -39,7 +44,7 @@ class Bill{
 
     public function delete(){ 
         $DBObject = new DB();
-        $sql = "DELETE FROM pm_o_v where id = '".$this->id."'";
+        $sql = "DELETE FROM bill where id = '".$this->id."'";
         $DBObject->connect();
         $DBObject->execute($sql);
         $DBObject->disconnect();
--- userside mostafa/WeekClass.php
+++ userside mostafa/WeekClass.php
@@ -2,18 +2,44 @@
 require_once("db.php");
 class week{
     public $id;
-    public $day;
+    public $days;
 
-    public function select(){
+
+    public function insert(){ 
+        $DBobject = new DB();
+        $sql = "INSERT INTO `week` (`days`) VALUES ('".$this->days."')";
+        $DBobject->connect();
+        $DBobject->execute($sql);
+        $DBobject->disconnect();
+      
+      }
+      public function select(){
         $DBObject = new DB();
-        $sql = "SELECT * FROM week  " ; //add condition if you want one
+        $sql = "SELECT * FROM week"; //add condition if you want one
         $DBObject->connect();
         $result = $DBObject->execute($sql);
         while ($row = mysqli_fetch_array($result)){
            echo $row['id'];
-           echo $row['day'];
+           echo $row['days'];
        }
        $DBObject->disconnect();
      }
+  
+      public function delete(){ 
+        $DBobject = new DB();
+        $sql="DELETE FROM week where id = '".$this->id."'";
+        $DBobject->connect();
+        $DBobject->execute($sql);
+        $DBobject->disconnect();
+      
+        }
+      public function update(){ 
+        $DBobject = new DB();
+        $sql = "UPDATE week SET days='".$this->days."' WHERE id = '".$this->id."'";
+        $DBobject->connect();
+        $DBobject->execute($sql);
+        $DBobject->disconnect();
+        }
+
 }
 ?>
