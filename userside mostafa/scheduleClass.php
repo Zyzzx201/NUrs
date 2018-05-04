b81f287bdd6d0236750157c7fb8909f149993230
Zyzzx201 <36744908+Zyzzx201@users.noreply.github.com>
2018-05-04 16:07:07 +0200

    Add files via upload

--- userside mostafa/scheduleClass.php
+++ userside mostafa/scheduleClass.php
@@ -3,12 +3,13 @@ require_once("db.php");
 class Schedule{
     public $id;
     public $course_id;
+    public $childtype_id;
     public $start;
     public $end;
 
 	  public function insert(){
       $DBobject = new DB();
-      $sql = "INSERT INTO schedule (course_id, start, end) VALUES ('".$this->course_id."','".$this->start."','".$this->end."')";
+      $sql = "INSERT INTO schedule (course_id,childtype_id, start, end) VALUES ('".$this->course_id."','".$this->childtype_id."','".$this->start."','".$this->end."')";
       $DBobject->connect();
       $DBobject->execute($sql);
       $DBobject->disconnect();
@@ -16,12 +17,13 @@ class Schedule{
     }
     public function select(){ 
       $DBobject = new DB();
-      $sql = "SELECT * FROM schedule WHERE id = '".$this->course_id."' ";
+      $sql = "SELECT * FROM schedule WHERE course_id = '".$this->course_id."' OR childtype_id = '".$this->childtype_id."' OR id = '".$this->id."'  ";
       $DBobject->connect();
       $DBobject->execute($sql);
         while ($row = mysqli_fetch_array($result)){
           echo $row['id'];
           echo $row['course_id'];
+          echo $row['childtype_id'];
           echo $row['start'];
           echo $row['end'];
       }
@@ -39,7 +41,7 @@ class Schedule{
     }
     public function update(){ 
       $DBobject = new DB();
-      $sql="UPDATE schedule SET course_id =  ,start ='".$this->start."' ,end = '".$this->end."'  WHERE id = '".$this->course_id."' ";
+      $sql="UPDATE schedule SET course_id = '".$this->course_id."'  ,start ='".$this->start."' ,end = '".$this->end."', childtype_id ='".$this->childtype_id."'  WHERE course_id = '".$this->course_id."' ";
       $DBobject->connect();
       $DBobject->execute($sql);
       $DBobject->disconnect();
